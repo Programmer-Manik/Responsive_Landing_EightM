83b0a4dc198ee48292d1632088373d858d5cca6f
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-24 20:14:28 +0600

    Responsive Landing project start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+function App() {
+ 
+
+  return (
+    <>
+      <Button>button</Button>
+    </>
+  )
+}
+
+export default App
