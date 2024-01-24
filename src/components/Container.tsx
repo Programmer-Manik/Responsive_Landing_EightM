7bbdbd7aebe7685b598ee910e6626479d3397601
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-24 23:15:59 +0600

    Tailwind And Shadcn Customization

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 
 const Container = ({children}) => {
    return (
-      <div className="w-full max-w-[1220px] mx-auto bg-green-500 px-[20px] ">
+      <div className="w-full max-w-[1220px] mx-auto px-[20px] ">
          {children}
       </div>
    );
--- src/components/ui/Layouts/Navbar.tsx
+++ src/components/ui/Layouts/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
       <header className="">
-        <nav className="w-full max-w-[1220px] mx-auto bg-green-500 h-full px-[20px] flex justify-between items-center">
+        <nav className="w-full max-w-[1220px] mx-auto h-full px-[20px] flex justify-between items-center">
           <span className="text-3xl">iManik</span>
           <ul className="space-x-5">
             <NavLink to="/">Home</NavLink>
