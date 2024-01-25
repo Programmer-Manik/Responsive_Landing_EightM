81cdb17beb1ba00800f4f9dbb43eb703156eaba0
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-25 20:42:54 +0600

    Fixed Navbar

--- src/components/ui/Layouts/Navbar.tsx
+++ src/components/ui/Layouts/Navbar.tsx
@@ -6,15 +6,16 @@ import {motion} from "framer-motion"
 const Navbar = () => {
   return (
     <div>
-      <motion.header className="py-5"
+      <motion.header className="h-16 fixed w-full bg-white z-[999]"
       initial={{opacity:0,}}
       animate={{opacity:1}}
       transition={{
-        duration:1
+        delay:1,
+        duration:1,
       }}
       >
-        <nav className="w-full max-w-[1220px] mx-auto h-full px-[20px] flex justify-between items-center">
-          <span className="text-3xl">iRepair</span>
+        <nav className="w-full max-w-[1220px] mx-auto h-full px-[20px]  flex justify-between items-center">
+          <span className="text-3xl">iManik</span>
           <ul className="space-x-5">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/About">About</NavLink>
