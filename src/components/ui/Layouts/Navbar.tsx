05b0cb99acbe7d6f1766614fc9fd9db513785273
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-25 09:51:57 +0600

    Responsive Hero Section

--- src/components/ui/Layouts/Navbar.tsx
+++ src/components/ui/Layouts/Navbar.tsx
@@ -4,9 +4,9 @@ import { Button } from "../button";
 const Navbar = () => {
   return (
     <div>
-      <header className="">
+      <header className="py-5">
         <nav className="w-full max-w-[1220px] mx-auto h-full px-[20px] flex justify-between items-center">
-          <span className="text-3xl">iManik</span>
+          <span className="text-3xl">iRepair</span>
           <ul className="space-x-5">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/About">About</NavLink>
--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -4,21 +4,21 @@ import MacBook from "@/assets/images/MacBook.jpg";
 
 const HeroSection = () => {
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-2 place-content-center">
+    <Container className=" h-[calc(100vh-80px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
       <div>
-        <h1 className="text-8xl font-bold ">
+        <h1 className="text-5xl lg:text-8xl font-bold text-nowrap">
           <span className="text-gray">Don't worry,</span>
           <br />
           <span>We'll fix it.</span>
         </h1>
-        <p>
-          Welcome to <span>iRepair</span>, your one-stop place for all kinds of{" "}
-          <span>MacBook repairs</span> and diagnostics.
+        <p className="max-w-[50ch] text-lg mt-10 mb-5 text-dark-gray ">
+          Welcome to{""} <span className="text-primary-foreground font-semibold">iRepair</span>{""}, your one-stop place for all kinds of{" "}
+          <span className="text-primary-foreground font-semibold">MacBook repairs</span>{""} and diagnostics.
         </p>
         <Button>Book a Service</Button>
       </div>
-      <div>
-        <img className="w-full rounded-lg" src={MacBook} alt="" />
+      <div className="mt-10 w-3/7 lg:w-full  mx-auto">
+        <img className="w-[90%] rounded-3xl -rotate-[30deg] h-[95%] object-contain" src={MacBook} alt="" />
       </div>
     </Container>
   );
