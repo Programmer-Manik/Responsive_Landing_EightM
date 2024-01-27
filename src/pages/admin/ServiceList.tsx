f342d2fe446789dd8acb0c3c661a8652926679c2
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-28 00:06:02 +0600

    queryKey

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,8 +1,31 @@
+
+import { useQuery } from "@tanstack/react-query";
+// import { useEffect, useState } from "react";
 const ServiceList = () => {
- return (
- <div>
- <h2>Welcome to the ServiceList page</h2>
- </div>
- );
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState([])
+  // const [error, setError] = useState([])
+  // console.log(data);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/api/v1/services")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.data));
+  // }, []);
+
+
+  const info = useQuery({
+    queryKey:["services"],
+    queryFn:(() => {
+      return fetch("http://localhost:5000/api/v1/services")
+    })
+})
+console.log(info)
+
+
+  return (
+    <div>
+      <h2>Welcome to the ServiceList page</h2>
+    </div>
+  );
 };
 export default ServiceList;
