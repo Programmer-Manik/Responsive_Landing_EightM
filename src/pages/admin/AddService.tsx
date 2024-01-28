8ea522263c59b2ed0756e124c84cf25619003d3c
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-28 06:44:06 +0600

    Post Data Using TanStack Query

--- src/pages/admin/AddService.tsx
+++ src/pages/admin/AddService.tsx
@@ -1,10 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { FormEvent, useState } from "react";
 
 const AddService = () => {
-   return (
-      <div>
-         <h1>as</h1>
-      </div>
-   );
+  const [serviceName, setServiceName] = useState("");
+  
+  const {mutateAsync, isError , isSuccess} = useMutation({
+   mutationFn: async(data) => {
+      return await fetch('http//localhost:5000/api/v1/services', {
+         method:'POST',
+         body: JSON.stringify(data),
+         headers:{
+            'Content-Type' : 'application/json'
+         },
+      });
+   },
+  });
+  
+  console.log({isError,isSuccess})
+  
+  const handleSubmit = async(e: FormEvent) => {
+    e.preventDefault()
+
+    const serviceData = {
+      name:serviceName,
+      description:'Replace any dead chips',
+      devices:['MackBook pro','MacKBook air', 'iPad pro'],
+      price:500.0,
+    }
+
+   console.log(serviceData);
+   await mutateAsync(serviceData);
+   console.log('done')
+  };
+  
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setServiceName(e.target.value)} />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default AddService;
